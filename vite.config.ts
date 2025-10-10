9bbf82083b99bb8732e7ddadd09cf6f8cab95f55
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2025-10-10 19:54:03 +0300

    add svg to react component support + svgs for hardware diagrams

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import svgr from "vite-plugin-svgr"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
-    }),tailwindcss()
+    }),
+    tailwindcss(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: {
+        // ...
+      },
+
+      // esbuild options, to transform jsx to js
+      esbuildOptions: {
+        // ...
+      },
+
+      // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
+      include: "**/*.svg?react",
+
+      //  A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should ignore. By default no files are ignored.
+      exclude: "",
+    }),
   ],
   resolve: {
     alias: {
