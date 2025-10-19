8c3eee0022cace4b3e11454998625e451243fb88
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2025-10-19 21:16:49 +0300

    init MonacoEditor IDE For MIPS Code

--- src/components/ExecutionPanel.tsx
+++ src/components/ExecutionPanel.tsx
@@ -1,6 +1,52 @@
+import Editor, { type Monaco, type OnMount } from "@monaco-editor/react";
+import { useRef } from "react";
+type IStandaloneCodeEditor = Parameters<OnMount>[0];
+
 function ExecutionPanel() {
-// todo: some work her needs to be done.
-  return <></>;
+  // todo: some work her needs to be done.
+
+  const editorRef = useRef<IStandaloneCodeEditor>(null);
+
+  function handleEditorDidMount(editor: IStandaloneCodeEditor, monaco: Monaco) {
+    editorRef.current = editor;
+    // editor.getModel
+
+    /** Highlight specific Lines */
+
+    editor.createDecorationsCollection([
+      {
+        range: new monaco.Range(1, 3, 1, 10),
+        options: {
+          isWholeLine: true,
+          inlineClassName: "inlineHighlight"
+        },
+      },
+    ]);
+  }
+
+  return (
+    <div className="">
+      {/* <button onClick={showValue} /> */}
+      <Editor
+        height={"100vh"}
+        width={"450px"}
+        defaultLanguage="mips"
+        defaultValue="# Write your code here."
+        theme="vs-dark"
+        onMount={handleEditorDidMount}
+        options={{
+          // readOnly: true,
+          minimap: { enabled: false },
+          overviewRulerLanes: 0,
+          scrollbar: {
+            vertical: "hidden",
+            horizontal: "hidden",
+            handleMouseWheel: false,
+          },
+        }}
+      />
+    </div>
+  );
 }
 
 export default ExecutionPanel;
