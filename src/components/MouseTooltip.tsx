3e209d57dba17bf87fac8da0e4ebcf2beabda8c2
Ron Tseytlin <ronts390@gmail.com>
2025-10-20 14:29:34 +0300

    hide mouse tooltip before mouse has moved

--- src/components/MouseTooltip.tsx
+++ src/components/MouseTooltip.tsx
@@ -6,23 +6,29 @@ import { Card } from "./ui/card"
  * A tooltip that follows the mouse.
  */
 export function MouseTooltip(props: { children: ReactNode }) {
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
+  // On the first render of this component, a `mousemove` event hasn't fired yet -
+  // so we wait for the first mouse movement before showing the component.
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0, moved: false })
+
   useEffect(() => {
     const listener = (e: MouseEvent) => {
-      setMousePos({ x: e.screenX, y: e.screenY })
+      setMousePos({ x: e.screenX, y: e.screenY, moved: true })
     }
     window.addEventListener("mousemove", listener)
     return () => {
       window.removeEventListener("mousemove", listener)
     }
   }, [])
-  return createPortal(
-    <Card
-      className="w-fit p-2 absolute left-0 top-0"
-      style={{ left: mousePos.x, top: mousePos.y }}
-    >
-      {props.children}
-    </Card>,
-    document.body,
-  )
+
+  return mousePos.moved
+    ? createPortal(
+        <Card
+          className="w-fit p-2 absolute left-0 top-0"
+          style={{ left: mousePos.x, top: mousePos.y }}
+        >
+          {props.children}
+        </Card>,
+        document.body,
+      )
+    : undefined
 }
