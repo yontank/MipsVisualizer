0d8d71d87fda796d0e1060c733557fe4a0edaf3e
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2025-10-20 16:44:10 +0300

    added utils for number conversion int to hex, etc.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,20 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { type hexadecimal } from "@/../types";
+import { type Hexadecimal } from "@/../types";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-export function int2hex(number: number): hexadecimal {
+
+export function int2hex(number: number, numDigits: number = 8): Hexadecimal {
   if (number < 0) {
     number = 0xffffffff + number + 1;
   }
 
   const hexValue = number.toString(16).toUpperCase();
-  const paddedHex = hexValue.padStart(8, "0");
-  return ("0x" + paddedHex) as hexadecimal;
-
+  const paddedHex = hexValue.padStart(numDigits, "0");
+  return ("0x" + paddedHex) as Hexadecimal;
 }
 
-export function hex2int(x: hexadecimal) {
+export function hex2int(x: Hexadecimal) {
   return Number(x);
 }
