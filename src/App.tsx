e810201fb00df9842b8a0e057f0766e0536c66d4
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2025-10-21 16:32:59 +0300

    feat: add tabbed interface for ExecutionDisplay and EmulatorTableUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import RegMemViewer from "./components/RegMemViewer"
 import DebugUI from "@/components/DebugUI"
 import ExecutionDisplay from "@/components/ExecutionPanel"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./components/ui/tabs"
+import EmulatorTableUI from "./components/MarsEmulatorUI/page"
 function App() {
   return (
     <>
@@ -10,8 +12,23 @@ function App() {
       </div>
 
       <div className="flex h-screen">
-        <ExecutionDisplay />
+        <Tabs defaultValue="IDE">
+          <TabsList className="w-full">
+            <TabsTrigger className="w-1/2 text-center" value="IDE">
+              Editor
+            </TabsTrigger>
+            <TabsTrigger className="w-1/2 text-center" value="debugger">
+              Execution
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="IDE">
+            <ExecutionDisplay />
+          </TabsContent>
 
+          <TabsContent value="debugger">
+            <EmulatorTableUI />
+          </TabsContent>
+        </Tabs>
         <div className="bg-blue-500 flex-1 overflow-auto resize-x min-w-36">
           Diagram
         </div>
