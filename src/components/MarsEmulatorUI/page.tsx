2c4027d988f4e190380a0e9cb733de449699d567
Yehonatan Avrahimi <48332126+yontank@users.noreply.github.com>
2025-10-23 13:04:11 +0300

    feature: add dynamic row styling to DataTable component

--- src/components/MarsEmulatorUI/page.tsx
+++ src/components/MarsEmulatorUI/page.tsx
@@ -7,26 +7,26 @@ function getData(): CompiledMIPS[] {
   // Fetch data from your API here.
   return [
     {
-      id: "728ed52f",
+      id: "",
       address: "0x00400000",
+      code: "0x1234567",
+      source: "add $6 , $2, $1",
+    },
+    {
+      id: "728ed52f",
+      address: "0x00400004",
+      code: "0x3C010000",
+      source: "sw $at, 0x0000",
+    },
+    {
+      id: "728ed52f",
+      address: "0x00400008",
       code: "0x3C010000",
       source: "lui $at, 0x0000",
     },
     {
       id: "728ed52f",
-      address: "0x00400000",
-      code: "0x3C010000",
-      source: "lui $at, 0x0000",
-    },
-    {
-      id: "728ed52f",
-      address: "0x00400000",
-      code: "0x3C010000",
-      source: "lui $at, 0x0000",
-    },
-    {
-      id: "728ed52f",
-      address: "0x00400000",
+      address: "0x0040000C",
       code: "0x3C010000",
       source: "lui $at, 0x0000",
     },
@@ -38,7 +38,15 @@ export default function DemoPage() {
 
   return (
     <div className="container mx-auto py-10 w-[375px]">
-      <DataTable columns={columns} data={data} />
+      <DataTable
+        columns={columns}
+        data={data}
+        setRowStyle={(row) => {
+          const address = row.original.address
+          if (address === "0x00400000") return { backgroundColor: "teal" }
+          return {}
+        }}
+      />
     </div>
   )
 }
